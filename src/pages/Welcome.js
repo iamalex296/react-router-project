81cb9cd0699c07544a08a608d33420a8e76fa6ed
alex <alexizana10@gmail.com>
2021-06-21 19:44:09 +0400

    added nested routes and redarecting

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,14 @@
+import { Route } from "react-router-dom";
+
 const Welcome = () => {
-  return <h1>Welcome Page</h1>;
+  return (
+    <section>
+      <h1>Welcome Page</h1>
+      <Route path="/welcome/new-user">
+        <p>WELCOME, NEW USER!</p>
+      </Route>
+    </section>
+  );
 };
 
 export default Welcome;
